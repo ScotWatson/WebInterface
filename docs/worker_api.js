e1650ce613c6d28983016d35e9734223593db656
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-01-26 19:56:49 -0500

    Update worker_api.js

--- docs/worker_api.js
+++ docs/worker_api.js
@@ -3,6 +3,19 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+function delete_member(obj, member) {
+  if (obj.hasOwnProperty(member)) {
+    return (delete obj[member]);
+  } else {
+    let prototype = Object.getPrototypeOf(obj);
+    if (prototype === null) {
+      return false;
+    }
+    delete_member(prototype, member);
+  }
+}
+// self.indexedDB.databases gives access to all databases on the current domain.  This is a security risk.
+delete_member(self.indexedDB, "databases");
 const objSystem = (function () {
   let me = {};
   let mapMessage = new Map();
