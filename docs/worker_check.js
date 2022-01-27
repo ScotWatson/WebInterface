ffdaf232cf821ee24ffa51099e3e5cbb0aa734c7
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-01-26 22:00:46 -0500

    Update worker_check.js

--- docs/worker_check.js
+++ docs/worker_check.js
@@ -19,3 +19,8 @@ function check_object(obj) {
 
 let arrNonVettedSelfMembers = check_object(self);
 self.postMessage(arrNonVettedSelfMembers);
+
+let myCrypto = new self.Crypto();
+let bufRandom = new Uint8Array(32);
+myCrypto.getRandomValues(bufRandom);
+self.postMessage(null, bufRandom);
