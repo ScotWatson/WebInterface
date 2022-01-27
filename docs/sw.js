027c446c19993d27d406c98c889845b15fe59051
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-01-26 20:24:22 -0500

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,14 +1,15 @@
-// (c) 2022 Scot Watson  All Rights Reserved
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+/*
+(c) 2022 Scot Watson  All Rights Reserved
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
-// NOTE: This path is hardcoded, as the service worker cannot access window.location
-const pathname = "/WebInterface/";
-
-function projectFile(filename) {
+function project_file(filename) {
+  // NOTE: This path is hardcoded, as the service worker cannot access window.location
+  const pathname = "/WebInterface/";
   return pathname + filename;
 }
 
-function selfInstall(e) {
+function self_install(e) {
   console.log("sw.js: Start Installing");
   function addCaches(cache) {
     console.log("sw.js: Start Adding Caches");
@@ -24,14 +25,18 @@ function selfInstall(e) {
   console.log("sw.js: End Installing");
 }
 
-function selfFetch(e) {
-  console.log("sw.js: Start Handling Fethch");
+function set_as_worker_script(path) {
+}
+
+function self_fetch(e) {
+  console.log("sw.js: Start Handling Fetch");
+  console.log(e);
   function sendResponse(response) {
     return response || fetch(e.request);
   }
   e.respondWith(caches.match(e.request).then(sendResponse));
-  console.log("sw.js: End Handling Fethch");
+  console.log("sw.js: End Handling Fetch");
 }
 
-self.addEventListener("install", selfInstall);
-self.addEventListener("fetch", selfFetch);
+self.addEventListener("install", self_install);
+self.addEventListener("fetch", self_fetch);
