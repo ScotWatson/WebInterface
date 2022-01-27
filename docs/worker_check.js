580b8ca53a9218061dd3bc40248d46dd25d0681f
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-01-26 21:51:55 -0500

    Update worker_check.js

--- docs/worker_check.js
+++ docs/worker_check.js
@@ -3,21 +3,19 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-const arrVettedSelfMembers = [ "indexedDB", "isSecureContext", "atob", "btoa", "clearInterval", "clearTimeout", "createImageBitmap", "fetch", "setInterval", "setTimeout", "reportError" ];
+const arrVettedSelfMembers = [];
+// [ "indexedDB", "isSecureContext", "atob", "btoa", "clearInterval", "clearTimeout", "createImageBitmap", "fetch", "setInterval", "setTimeout", "reportError" ];
 
-function check_object(obj, arrVettedMembers) {
-  let arrNonVettedMembers = [];
-  for (let member of Object.getOwnPropertyNames(obj)) {
-    if (!(arrVettedMembers.includes(member))) {
-      arrNonVettedMembers.push(member);
-    }
-  }
+function check_object(obj) {
+  let obj_members = Object.getOwnPropertyNames(obj);
   let prototype = Object.getPrototypeOf(obj);
-  if (prototype !== null) {
-    check_object(prototype, arrVettedMembers);
+  if (prototype === null) {
+    return { obj: obj_members };
+  } else {
+    let prototype_members = check_object(prototype);
+    return { obj: obj_members, prototype_obj: prototype_members };
   }
-  return arrNonVettedMembers;
 }
 
-let arrNonVettedSelfMembers = check_object(self, arrVettedSelfMembers);
+let arrNonVettedSelfMembers = check_object(self);
 self.postMessage(arrNonVettedSelfMembers);
