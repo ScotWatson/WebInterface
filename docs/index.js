720c98092c38fc4d881498a0b17d2d9b853c3df5
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-09-30 21:34:24 -0400

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -188,17 +188,6 @@ msthumbnailclick
 
 let user;
 
-let divScreenSize;
-let divScreenAvailWidth;
-let divScreenAvailHeight;
-let divClientWidth;
-let divClientHeight;
-let divInnerWidth;
-let divInnerHeight;
-let divScrollWidth;
-let divScrollHeight;
-let divWindowSize;
-
 function start( [ evtWindow, moduleErrorHandling ] ) {
   window.addEventListener("resize", resize);
   switch (mode) {
@@ -215,100 +204,22 @@ function start( [ evtWindow, moduleErrorHandling ] ) {
       break;
   }
   document.body.style.margin = "0";
+  document.body.style.overflow = "hidden";
+  document.body.style.backgroundColor = "#808080";
   const inp = document.createElement("input");
+  inp.style.display = "block";
+  inp.style.position = "absolute";
+  inp.style.left = "0px";
+  inp.style.top = "0px";
+  inp.style.width = "100%";
+  inp.style.height = "50px";
+  inp.setAttribute("placeholder", "Username");
   document.body.appendChild(inp);
-  divScreenSize = document.createElement("div");
-  document.body.appendChild(divScreenSize);
-  divScreenAvailWidth = document.createElement("div");
-  document.body.appendChild(divScreenAvailWidth);
-  divScreenAvailHeight = document.createElement("div");
-  document.body.appendChild(divScreenAvailHeight);
-  divClientWidth = document.createElement("div");
-  divClientWidth.style.backgroundColor = "#0000FF";
-  document.body.appendChild(divClientWidth);
-  divClientHeight = document.createElement("div");
-  divClientHeight.style.backgroundColor = "#0000FF";
-  document.body.appendChild(divClientHeight);
-  divInnerWidth = document.createElement("div");
-  document.body.appendChild(divInnerWidth);
-  divInnerHeight = document.createElement("div");
-  document.body.appendChild(divInnerHeight);
-  divScrollWidth = document.createElement("div");
-  document.body.appendChild(divScrollWidth);
-  divScrollHeight = document.createElement("div");
-  document.body.appendChild(divScrollHeight);
-  document.body.style.backgroundColor = "#FFFFFF";
-  
-  document.body.appendChild(document.createElement("br"));
-  const btnOverflowAuto = document.createElement("button");
-  btnOverflowAuto.addEventListener("click", function (evt) {
-    document.body.style.overflow = "auto";
-  });
-  btnOverflowAuto.innerHTML = "Auto";
-  document.body.appendChild(btnOverflowAuto);
-  const btnOverflowHidden = document.createElement("button");
-  btnOverflowHidden.addEventListener("click", function (evt) {
-    document.body.style.overflow = "hidden";
-  });
-  btnOverflowHidden.innerHTML = "Hidden";
-  document.body.appendChild(btnOverflowHidden);
-  const btnOverflowNone = document.createElement("button");
-  btnOverflowNone.addEventListener("click", function (evt) {
-    document.body.style.overflow = "none";
-  });
-  btnOverflowNone.innerHTML = "None";
-  document.body.appendChild(btnOverflowNone);
-  document.body.appendChild(document.createElement("br"));
-  document.body.appendChild(document.createElement("br"));
-  const btnResize = document.createElement("button");
-  btnResize.addEventListener("click", resize);
-  btnResize.innerHTML = "Resize";
-  document.body.appendChild(btnResize);
-  const btnFullscreen = document.createElement("button");
-  btnFullscreen.addEventListener("click", function (evt) {
-    if (document.fullscreenElement === null) {
-      document.documentElement.requestFullscreen();
-    } else {
-      document.exitFullscreen();
-    }
-  });
-  btnFullscreen.innerHTML = "Fullscreen";
-  document.body.appendChild(btnFullscreen);
-  const divResize = document.createElement("div");
-  divResize.style.backgroundColor = "#808080";
-  divResize.style.width = "50px";
-  divResize.style.height = "100px";
-  divResize.addEventListener("click", function (evt) {
-    if (divResize.style.height === "1000px") {
-      divResize.style.height = "100px";
-    } else {
-      divResize.style.height = "1000px";
-    }
-  });
-  document.body.appendChild(divResize);
-  divWindowSize = document.createElement("div");
-  divWindowSize.style.position = "absolute";
-  divWindowSize.style.left = "0px";
-  divWindowSize.style.top = "0px";
-  divWindowSize.style.opacity = 0.25;
-  divWindowSize.style.backgroundColor = "blue";
-  divWindowSize.style.zIndex = "-1";
-  document.body.appendChild(divWindowSize);
   resize();
 }
 
 function resize() {
-  divScreenSize.innerHTML = "screen size = " + screen.width + " x " + screen.height;
-  divScreenAvailWidth.innerHTML = "screen.availWidth = " + screen.availWidth;
-  divScreenAvailHeight.innerHTML = "screen.availHeight = " + screen.availHeight;
-  divClientWidth.innerHTML = "document.documentElement.clientWidth = " + document.documentElement.clientWidth;
-  divClientHeight.innerHTML = "document.documentElement.clientHeight = " + document.documentElement.clientHeight;
-  divInnerWidth.innerHTML = "window.innerWidth = " + window.innerWidth;
-  divInnerHeight.innerHTML = "window.innerHeight = " + window.innerHeight;
-  divScrollWidth.innerHTML = "document.body.scrollWidth = " + document.body.scrollWidth;
-  divScrollHeight.innerHTML = "document.body.scrollHeight = " + document.body.scrollHeight;
-  divWindowSize.style.width = window.innerWidth + "px";
-  divWindowSize.style.height = window.innerHeight + "px";
+//  window.innerWidth + window.innerHeight;
 }
 
 function loginScreen() {
