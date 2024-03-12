63153fbe3ece4a991aba5f5c81c51626892299cc
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-03-12 19:38:02 -0400

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,7 +1,12 @@
 /*
-(c) 2022 Scot Watson  All Rights Reserved
+(c) 2024 Scot Watson  All Rights Reserved
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
+
+const urlSelf = new URL(self.location);
+const urlTestGame = new URL("./TestGame/", urlSelf);
+const urlClasses = new URL("./classes.js", urlSelf);
+const pathnameSegments = urlSelf.pathname.split("/");
 
 function project_file(filename) {
   // NOTE: This path is hardcoded, as the service worker cannot access window.location
@@ -14,11 +19,11 @@ function self_install(e) {
   function addCaches(cache) {
     console.log("sw.js: Start Adding Caches");
     cache.addAll([
-      project_file(""),
-      project_file("index.html"),
-      project_file("index.js"),
-      project_file("worker_api.js"),
-      project_file("style.css"),
+      project_file("./"),
+      project_file("./index.html"),
+      project_file("./index.js"),
+      project_file("./worker_api.js"),
+      project_file("./style.css"),
     ])
     console.log("sw.js: End Adding Caches");
   }
