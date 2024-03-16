a7343adf67510814bde95f6b13cec2c2bee93e7f
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-03-16 17:14:00 -0400

    Update init.js

--- docs/init.js
+++ docs/init.js
@@ -1,5 +1,5 @@
 "use strict";
-import * from "/common.js";
+const moduleCommon = import("./common.js");
 
 self.init = function init({
   latestVersion,
@@ -8,30 +8,32 @@ self.init = function init({
   const selfURL = new URL(window.location);
   const requestedVersion = selfURL.searchParams.get("version");
   self._siteURI = siteURL;
-  window.siteSessionStorage = new SiteStorage({
-    uri: self._siteURI(),
-    storage: window.sessionStorage,
+  moduleCommon.then(function (Common) {
+    window.siteSessionStorage = new Common.SiteStorage({
+      uri: self._siteURI(),
+      storage: window.sessionStorage,
+    });
+    window.siteLocalStorage = new Common.SiteStorage({
+      uri: self._siteURI(),
+      storage: window.localStorage,
+    });
+    const storedVersion = window.siteSessionStorage.getItem("version");
+    if (requestedVersion !== null) {
+      window.siteSessionStorage.setItem("version", requestedVersion);
+      const newSearchParams = new self.URLSearchParams(selfURL.searchParams.toString());
+      newSearchParams.delete("version");
+      const newURL = new self.URL(selfURL.protocol + "//" + selfURL.host + selfURL.pathname + ((newSearchParams.size === 0) ? "" : "?" + newSearchParams.toString()) + selfURL.hash);
+      window.history.replaceState(null, "", newURL.toString());
+    } else if (storedVersion !== null) {
+      window.siteSessionStorage.setItem("version", storedVersion);
+    } else {
+      window.siteSessionStorage.setItem("version", latestVersion);
+    }
+    self._version = function () {
+      return window.siteSessionStorage.getItem("version");
+    }
+    const scriptElem = document.createElement("script");
+    scriptElem.src = "./" + self._version() + "/editor.js";
+    document.head.appendChild(scriptElem);
   });
-  window.siteLocalStorage = new SiteStorage({
-    uri: self._siteURI(),
-    storage: window.localStorage,
-  });
-  const storedVersion = window.siteSessionStorage.getItem("version");
-  if (requestedVersion !== null) {
-    window.siteSessionStorage.setItem("version", requestedVersion);
-    const newSearchParams = new URLSearchParams(selfURL.searchParams.toString());
-    newSearchParams.delete("version");
-    const newURL = new URL(selfURL.protocol + "//" + selfURL.host + selfURL.pathname + ((newSearchParams.size === 0) ? "" : "?" + newSearchParams.toString()) + selfURL.hash);
-    window.history.replaceState(null, "", newURL.toString());
-  } else if (storedVersion !== null) {
-    window.siteSessionStorage.setItem("version", storedVersion);
-  } else {
-    window.siteSessionStorage.setItem("version", latestVersion);
-  }
-  self._version = function () {
-    return window.siteSessionStorage.getItem("version");
-  }
-  const scriptElem = document.createElement("script");
-  scriptElem.src = "./" + self._version() + "/editor.js";
-  document.head.appendChild(scriptElem);
 }
