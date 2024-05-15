a78f11fa11782953c90cc8e0bed55283686d03d4
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-05-14 22:05:48 -0400

    Update WindowMessaging.mjs

--- docs/20240316/WindowMessaging.mjs
+++ docs/20240316/WindowMessaging.mjs
@@ -3,36 +3,44 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-window.addEventListener("message", messageHandler);
-const sourceHandlers = new Map();
-let unknownSourceHandler = function () {
-  throw "Received message from unrecognized source";
-}
-window.navigator.serviceWorker.addEventListener("message", function (evt) {
-  
-});
-
-triggerObj = {
-  trigger: function (obj) {
-    
-  },
-  executeOnTrigger: function () {
-    
-  },
-};
-
-function createSignal() {
+// async Iterable Iterator
+function createSignal(initFunc) {
   const obj = {};
-  obj.trigger = function (value) {
-    return;
+  let resolveArray = [];
+  let rejectArray = [];
+  let resolve = function (value, done) {
+    for (const resolve of resolveArray) {
+      resolve({
+        value: value,
+        done: !!done,
+      });
+    }
+    resolveArray = [];
+    rejectArray = [];
+  };
+  let reject = function (error) {
+    for (const reject of rejectArray) {
+      reject({
+        value: value,
+        done: true,
+      });
+    }
+    resolveArray = [];
+    rejectArray = [];
   };
   obj.next = function () {
     return new Promise(function (resolve, reject) {
-      resolve();
+      resolveArray.push(resolve);
+      rejectArray.push(reject);
     });
   };
+  obj[Symbol.asyncIterator] = obj.next;
+  initFunc(resolve, reject);
+  delete resolve;
+  delete reject;
   return obj;
 }
+
 function AbortablePromise(promiseFunction, abortFunction) {
   const ret = new Promise(function (resolve, reject) {
     promiseFunction(resolve, reject);
@@ -44,39 +52,63 @@ function AbortablePromise(promiseFunction, abortFunction) {
   return ret;
 }
 
-export const serviceWorkerMessageReceiver = (function () {
-  const obj = {};
-  
-  obj.
-  return obj;
-})();
+window.addEventListener("message", messageHandler);
+const sourceHandlers = new Map();
+let unknownSourceHandler;
+let unknownSourceSignal = createSignal(function (resolve, reject) {
+  unknownSourceMessage = resolve;
+});
+
+unknownSourceSignal.next().then(function () {
+  throw "Received message from unrecognized source";
+});
 
 function messageReceiver(evt) {
-  if (sources.has(evt.source)) {
-    const thisSource = sourceHandlers.get(evt.source);
-    thisSource._message(evt.data);
+  if (sourceHandlers.has(evt.source)) {
+    const thisHandler = sourceHandlers.get(evt.source);
+    thisHandler(evt.data);
   } else {
     unknownSourceHandler(evt);
   }
 }
 
-export function createWindowMessageSender({
-  destination,
-  origin,
-}) {
+export const serviceWorkerMessageReceiver = (function () {
   const obj = {};
+  let resolveFunc;
+  let rejectFunc;
+  obj.message = createSignal(function (resolve, reject) {
+    resolveFunc = resolve;
+    rejectFunc = reject;
+  });
+  window.navigator.serviceWorker.addEventListener("message", function (evt) {
+    resolveFunc(evt);
+  });
+  window.navigator.serviceWorker.addEventListener("messageerror", function (evt) {
+    rejectFunc(evt);
+  });
   return obj;
-}
+})();
 
 export function createMessageReceiver({
   source,
 }) {
   const obj = {};
+  obj.messageSignal = createSignal(function (resolve, reject) {
+    sourceHandlers.set(evt.source, resolve);
+  });
   return obj;
 }
 
-export function setUnknownSourceHandler({
-  handler,
+export function createWindowMessageSender({
+  window,
+  origin,
 }) {
-  unknownSourceHandler = handler;
+  const obj = {};
+  obj.send = function ({
+    data,
+    transferable,
+  ) {
+    window.postMessage(data, origin, transferable);
+  };
+  return obj;
 }
