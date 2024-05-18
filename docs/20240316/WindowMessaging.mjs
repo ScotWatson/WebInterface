1070fb813d9b9d9c2dd72aa425aaa06ec7a5ca20
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-05-18 10:23:20 -0400

    Update WindowMessaging.mjs

--- docs/20240316/WindowMessaging.mjs
+++ docs/20240316/WindowMessaging.mjs
@@ -34,7 +34,9 @@ function createSignal(initFunc) {
       rejectArray.push(reject);
     });
   };
-  obj[Symbol.asyncIterator] = obj.next;
+  obj[Symbol.asyncIterator] = function () {
+    return obj;
+  };
   initFunc(resolve, reject);
   resolve = null;
   reject = null;
