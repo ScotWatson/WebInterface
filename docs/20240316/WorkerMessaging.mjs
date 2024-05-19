f999ac8cb87f058a832ffe0a1ca9e111a829b5ce
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-05-19 18:34:17 -0400

    Create WorkerMessaging.mjs

---
+++ docs/20240316/WorkerMessaging.mjs
@@ -0,0 +1,20 @@
+/*
+(c) 2024 Scot Watson  All Rights Reserved
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+const Common = await import("https://scotwatson.github.io/WebInterface/common.js");
+
+const parentSource = Common.createSignal(function (resolve, reject) {
+  self.addEventListener("message", resolve);
+  self.addEventListener("messageerror", reject);
+});
+
+const parentSink = {
+  send: function ({
+    data,
+    transfer,
+  }) {
+    self.postMessage(data, transfer);
+  },
+}
