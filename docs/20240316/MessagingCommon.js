7d29ec68cbfdb87d438a36a574000d781b0e7988
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-05-19 19:01:30 -0400

    Create MessagingCommon.js

---
+++ docs/20240316/MessagingCommon.js
@@ -0,0 +1,130 @@
+/*
+(c) 2024 Scot Watson  All Rights Reserved
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+importScripts("https://scotwatson.github.io/WebInterface/common.js");
+
+export function createRemoteProcedureSocket({
+  messageSource,
+  messageSink,
+}) {
+  const obj = {};
+  const messageIds = new Map();
+  const responseFunctions = new Map();
+  obj.register = function ({
+    functionName,
+    handlerFunc,
+  }) {
+    responseFunctions.set(functionName, handlerFunc);
+  };
+  obj.unregister = function ({
+    functionName,
+  }) {
+    responseFunctions.delete(functionName);
+  };
+  // returns a promise, so acts as an async function
+  obj.call = function ({
+    functionName,
+    args,
+    transferable,
+  }) {
+    return new Promise(function (resolve, reject) {
+      const messageId = self.crypto.randomUUID();
+      messageIds.set(messageId, { resolve, reject });
+      messageSink.send({
+        data: {
+          id: messageId,
+          action: "request",
+          functionName: functionName,
+          args: args,
+        },
+        transferable: transferable,
+      });
+    });
+  };
+  (async function () {
+    for await (const data of messageSource.message) {
+      if (!data || !data.id || !data.action) {
+        messageSink.send({
+          data: {
+            id: data.id,
+            action: "error",
+            error: "Invalid Message",
+          },
+        });
+        continue;
+      }
+      switch (data.action) {
+        case "request": {
+          requestHandler(data);
+        }
+          break;
+        case "response": {
+          responseHandler(data);
+        }
+          break;
+        case "error": {
+          errorHandler(data);
+        }
+          break;
+        default:
+          messageSink.send({
+            data: {
+              id: data.id,
+              action: "error",
+              error: "Invalid Message",
+            },
+          });
+      }
+    }
+  })();
+  async function requestHandler(data) {
+    const thisFunction = responseFunctions.get(data.functionName);
+    if (typeof thisFunction !== "function") {
+      messageSink.send({
+        data: {
+          id: data.id,
+          action: "error",
+          reason: "Unregistered function: " + data.functionName,
+        },
+      });
+      return;
+    }
+    try {
+      data.args.transferable = [];
+      const ret = await thisFunction(data.args);
+      messageSink.send({
+        data: {
+          id: data.id,
+          action: "response",
+          value: ret,
+        },
+        transferable: data.args.transferable,
+      });
+    } catch (e) {
+      messageSink.send({
+        data: {
+          id: data.id,
+          action: "error",
+          error: e,
+        },
+      });
+    }
+  }
+  function responseHandler(data) {
+    const functions = messageIds.get(data.id);
+    if (functions !== undefined) {
+      functions.resolve(data.value);
+      messageIds.delete(data.id);
+    }
+  };
+  function errorHandler(data) {
+    const functions = messageIds.get(data.id);
+    if (functions !== undefined) {
+      functions.reject(data.reason);
+      messageIds.delete(data.id);
+    }
+  };
+  return obj;
+}
