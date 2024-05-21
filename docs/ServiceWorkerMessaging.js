0c72b1f3cd0998aa78ace0eb314584453ae18c87
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-05-21 19:52:50 -0400

    Update ServiceWorkerMessaging.js

--- docs/ServiceWorkerMessaging.js
+++ docs/ServiceWorkerMessaging.js
@@ -3,9 +3,13 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-importScripts("https://scotwatson.github.io/WebInterface/common.js");
+const export = {};
 
-const Messaging = {};
+const Common = (function () {
+  importScripts("https://scotwatson.github.io/WebInterface/common.js");
+  return export;
+})();
+
 const registeredClients = new Map();
 let unregisteredClientHandler;
 
@@ -25,7 +29,7 @@ const newClientMessage = Common.createSignal(function (resolve, reject) {
   unregisteredClientHandler = resolve;
 }),
 
-Messaging.createClientSource = function createClientSource({
+export.createClientSource = function createClientSource({
   client,
 }) {
   return {
@@ -35,7 +39,7 @@ Messaging.createClientSource = function createClientSource({
   };
 };
 
-Messaging.createClientSink = function createClientSink({
+export.createClientSink = function createClientSink({
   client,
 }) {
   return {
