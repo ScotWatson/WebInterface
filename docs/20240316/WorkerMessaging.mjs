936b27731983e291ae9bb651138c6946bbcd1041
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-05-22 18:29:57 -0400

    Update WorkerMessaging.mjs

--- docs/20240316/WorkerMessaging.mjs
+++ docs/20240316/WorkerMessaging.mjs
@@ -3,19 +3,16 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-const MessagingCommon = (function () {
-  importScripts("https://scotwatson.github.io/WebInterface/20240316/MessagingCommon.js");
-  return export;
-})();
+const MessagingCommon = await importScripts("https://scotwatson.github.io/WebInterface/20240316/messaging-common.mjs");
 
 export const createRemoteProcedureSocket = MessagingCommon.createRemoteProcedureSocket;
 
-const parentSource = Common.createSignal(function (resolve, reject) {
+export const parentSource = Common.createSignal(function (resolve, reject) {
   self.addEventListener("message", resolve);
   self.addEventListener("messageerror", reject);
 });
 
-const parentSink = {
+export const parentSink = {
   send: function ({
     data,
     transfer,
