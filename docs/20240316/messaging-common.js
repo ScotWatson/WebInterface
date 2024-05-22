026cf19edda386b4cb62c8fdb70019fbb6366aff
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-05-22 18:24:07 -0400

    Update messaging-common.js

--- docs/20240316/messaging-common.js
+++ docs/20240316/messaging-common.js
@@ -3,14 +3,14 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-const export = {};
+const exports = {};
 
 const Common = (function () {
   importScripts("https://scotwatson.github.io/WebInterface/common.js");
-  return export;
+  return exports;
 })();
 
-export.createRemoteProcedureSocket =  function createRemoteProcedureSocket({
+exports.createRemoteProcedureSocket =  function createRemoteProcedureSocket({
   messageSource,
   messageSink,
 }) {
