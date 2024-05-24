22ee2f940ccf3080215721fada8518c10eb6e1c0
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-05-23 22:03:56 -0400

    Update init.js

--- docs/init.js
+++ docs/init.js
@@ -1,69 +1,69 @@
+// Intended to be included as a script tag in an HTML file
+
 "use strict";
 
-window.$.load = new Promise(function (resolve, reject) {
-  window.addEventListener("load", function (evt) {
-    resolve(evt);
+document.currentScript.exports = (function () {
+  const exports = {};
+  exports.load = new Promise(function (resolve, reject) {
+    window.addEventListener("load", function (evt) {
+      resolve(evt);
+    });
   });
-});
-
-//If there is no controller, wait for one before proceeding
-window.$.controller = new Promise(function (resolve, reject) {
-  if (navigator.serviceWorker.controller !== null) {
-    resolve();
-    return;
+  exports.controller = new Promise(function (resolve, reject) {
+    if (navigator.serviceWorker.controller !== null) {
+      resolve();
+      return;
+    }
+    navigator.serviceWorker.addEventListener("controllerchange", function (evt) {
+      resolve();
+      return;
+    });
+  });
+  // Obtain initialization info
+  exports.selfUrl = new URL(self.location);
+  exports.serviceWorkerUrl = new URL("./sw.js", urlSelf);
+  exports.serviceWorkerScopeUrl = new URL("./", urlSelf);
+  // Register the service worker.
+  exports.registrationPromise = window.navigator.serviceWorker.register(serviceWorkerUrl.href, {
+    scope: serviceWorkerScopeUrl.href,
+  });
+  exports.init = function({
+    latestVersion,
+    siteURI
+  }) {
+    const requestedVersion = exports.selfUrl.searchParams.get("version");
+    self._siteURI = siteURI;
+    import("./common.mjs").then(function (Common) {
+      window.siteSessionStorage = new Common.SiteStorage({
+        uri: self._siteURI(),
+        storage: window.sessionStorage,
+      });
+      window.siteLocalStorage = new Common.SiteStorage({
+        uri: self._siteURI(),
+        storage: window.localStorage,
+      });
+      const storedVersion = window.siteSessionStorage.get("version");
+      if (requestedVersion !== null) {
+        window.siteSessionStorage.set("version", requestedVersion);
+        const newSearchParams = new self.URLSearchParams(exports.selfUrl.searchParams.toString());
+        newSearchParams.delete("version");
+        const newURL = new self.URL(exports.selfUrl.protocol + "//" + exports.selfUrl.host + exports.selfUrl.pathname + ((newSearchParams.size === 0) ? "" : "?" + newSearchParams.toString()) + exports.selfUrl.hash);
+        window.history.replaceState(null, "", newURL.toString());
+      } else if (storedVersion !== null) {
+        window.siteSessionStorage.set("version", storedVersion);
+      } else {
+        window.siteSessionStorage.set("version", latestVersion);
+      }
+      self._version = function () {
+        return window.siteSessionStorage.get("version");
+      }
+      const styleElem = document.createElement("link");
+      styleElem.href = "./" + self._version() + "/style.css";
+      styleElem.rel = "stylesheet";
+      document.head.appendChild(styleElem);
+      const scriptElem = document.createElement("script");
+      scriptElem.src = "./" + self._version() + "/index.js";
+      document.head.appendChild(scriptElem);
+    });
   }
-  navigator.serviceWorker.addEventListener("controllerchange", function (evt) {
-    resolve();
-    return;
-  });
-});
-
-// Obtain initialization info
-window.$.selfUrl = new URL(self.location);
-window.$.serviceWorkerUrl = new URL("./sw.js", urlSelf);
-window.$.serviceWorkerScopeUrl = new URL("./", urlSelf);
-// Register the service worker.
-window.$.registrationPromise = window.navigator.serviceWorker.register(serviceWorkerUrl.href, {
-  scope: serviceWorkerScopeUrl.href,
-});
-
-window.$.init = function({
-  latestVersion,
-  siteURI
-}) {
-  const selfURL = new URL(window.location);
-  const requestedVersion = selfURL.searchParams.get("version");
-  self._siteURI = siteURI;
-  moduleCommon.then(function (Common) {
-    window.siteSessionStorage = new Common.SiteStorage({
-      uri: self._siteURI(),
-      storage: window.sessionStorage,
-    });
-    window.siteLocalStorage = new Common.SiteStorage({
-      uri: self._siteURI(),
-      storage: window.localStorage,
-    });
-    const storedVersion = window.siteSessionStorage.get("version");
-    if (requestedVersion !== null) {
-      window.siteSessionStorage.set("version", requestedVersion);
-      const newSearchParams = new self.URLSearchParams(selfURL.searchParams.toString());
-      newSearchParams.delete("version");
-      const newURL = new self.URL(selfURL.protocol + "//" + selfURL.host + selfURL.pathname + ((newSearchParams.size === 0) ? "" : "?" + newSearchParams.toString()) + selfURL.hash);
-      window.history.replaceState(null, "", newURL.toString());
-    } else if (storedVersion !== null) {
-      window.siteSessionStorage.set("version", storedVersion);
-    } else {
-      window.siteSessionStorage.set("version", latestVersion);
-    }
-    self._version = function () {
-      return window.siteSessionStorage.get("version");
-    }
-    const styleElem = document.createElement("link");
-    styleElem.href = "./" + self._version() + "/style.css";
-    styleElem.rel = "stylesheet";
-    document.head.appendChild(styleElem);
-    const scriptElem = document.createElement("script");
-    scriptElem.src = "./" + self._version() + "/index.js";
-    document.head.appendChild(scriptElem);
-  });
-}
+})();
