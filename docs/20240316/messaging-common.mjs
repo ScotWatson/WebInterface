e1e5919459e1bcff6c566eb909c686cebd105494
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-05-24 15:56:25 -0400

    Update messaging-common.mjs

--- docs/20240316/messaging-common.mjs
+++ docs/20240316/messaging-common.mjs
@@ -9,6 +9,7 @@ export function createRemoteProcedureSocket({
   messageSource,
   messageSink,
 }) {
+  console.log(messageSource, messageSink);
   const obj = {};
   const messageIds = new Map();
   const responseFunctions = new Map();
