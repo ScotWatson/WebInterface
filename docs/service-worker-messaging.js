3d45e82b248c25b946ce38a7eea493627b76fd2c
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-05-25 10:58:24 -0400

    Update service-worker-messaging.js

--- docs/service-worker-messaging.js
+++ docs/service-worker-messaging.js
@@ -3,56 +3,45 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-//const exports = {};
-
-const Common = (function () {
-  importScripts("https://scotwatson.github.io/WebInterface/common.js");
+self.currentScript.exports = (function () {
+  const exports = {};
+  const Common = self.importScript("https://scotwatson.github.io/WebInterface/common.js");
+  const registeredClients = new Map();
+  let unregisteredClientHandler;
+  self.addEventListener("message", function (evt) {
+    const thisClient = registeredClients.get(evt.source);
+    if (thisClient) {
+      for (const source of thisClient.sources) {
+        source(evt);
+      }
+    } else {
+      unregisteredClientHandler(evt);
+    }
+  });
+  self.addEventListener("messageerror", console.error);
+  const newClientMessage = Common.createSignal(function (resolve, reject) {
+    unregisteredClientHandler = resolve;
+  }),
+  exports.createClientSource = function createClientSource({
+    client,
+  }) {
+    return {
+      message: Common.createSignal(function (resolve, reject) {
+        registeredClients.set(client, resolve);
+      }),
+    };
+  };
+  exports.createClientSink = function createClientSink({
+    client,
+  }) {
+    return {
+      send: function ({
+        data,
+        transfer,
+      }) {
+        client.postMessage(data, transfer);
+      },
+    }
+  };
   return exports;
 })();
-
-console.log(self.Common);
-
-/*
-
-const registeredClients = new Map();
-let unregisteredClientHandler;
-
-self.addEventListener("message", function (evt) {
-  const thisClient = registeredClients.get(evt.source);
-  if (thisClient) {
-    for (const source of thisClient.sources) {
-      source(evt);
-    }
-  } else {
-    unregisteredClientHandler(evt);
-  }
-});
-self.addEventListener("messageerror", console.error);
-
-const newClientMessage = Common.createSignal(function (resolve, reject) {
-  unregisteredClientHandler = resolve;
-}),
-
-exports.createClientSource = function createClientSource({
-  client,
-}) {
-  return {
-    message: Common.createSignal(function (resolve, reject) {
-      registeredClients.set(client, resolve);
-    }),
-  };
-};
-
-exports.createClientSink = function createClientSink({
-  client,
-}) {
-  return {
-    send: function ({
-      data,
-      transfer,
-    }) {
-      client.postMessage(data, transfer);
-    },
-  }
-};
-*/
