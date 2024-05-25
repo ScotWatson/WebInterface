e6118622821444d78cbe7783a33499c4246f9022
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-05-25 10:48:18 -0400

    Update common.js

--- docs/common.js
+++ docs/common.js
@@ -3,119 +3,122 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-const exports = {};
-// async Iterable Iterator
-exports.createSignal = function createSignal(initFunc) {
-  const obj = {};
-  let resolveArray = [];
-  let rejectArray = [];
-  let resolve = function (value, done) {
-    for (const resolve of resolveArray) {
-      resolve({
-        value: value,
-        done: !!done,
+self.currentScript.exports = (function() {
+  const exports = {};
+  // async Iterable Iterator
+  exports.createSignal = function createSignal(initFunc) {
+    const obj = {};
+    let resolveArray = [];
+    let rejectArray = [];
+    let resolve = function (value, done) {
+      for (const resolve of resolveArray) {
+        resolve({
+          value: value,
+          done: !!done,
+        });
+      }
+      resolveArray = [];
+      rejectArray = [];
+    };
+    let reject = function (error) {
+      for (const reject of rejectArray) {
+        reject({
+          value: value,
+          done: true,
+        });
+      }
+      resolveArray = [];
+      rejectArray = [];
+    };
+    obj.next = function () {
+      return new Promise(function (resolve, reject) {
+        resolveArray.push(resolve);
+        rejectArray.push(reject);
       });
-    }
-    resolveArray = [];
-    rejectArray = [];
-  };
-  let reject = function (error) {
-    for (const reject of rejectArray) {
-      reject({
-        value: value,
-        done: true,
-      });
-    }
-    resolveArray = [];
-    rejectArray = [];
-  };
-  obj.next = function () {
-    return new Promise(function (resolve, reject) {
-      resolveArray.push(resolve);
-      rejectArray.push(reject);
-    });
-  };
-  obj[Symbol.asyncIterator] = function () {
+    };
+    obj[Symbol.asyncIterator] = function () {
+      return obj;
+    };
+    initFunc(resolve, reject);
+    resolve = null;
+    reject = null;
     return obj;
   };
-  initFunc(resolve, reject);
-  resolve = null;
-  reject = null;
-  return obj;
-};
-exports.createAbortablePromise = function createAbortablePromise({
-  initFunc,
-  abortFunc,
-}) {
-  let resolveFunc;
-  let rejectFunc;
-  const ret = new Promise(function (resolve, reject) {
-    resolveFunc = resolve;
-    rejectFunc = reject;
-    initFunc(resolve, reject);
-  });
-  ret.resolve = function (value) {
-    abortFunc();
-    resolveFunc(value);
+  exports.createAbortablePromise = function createAbortablePromise({
+    initFunc,
+    abortFunc,
+  }) {
+    let resolveFunc;
+    let rejectFunc;
+    const ret = new Promise(function (resolve, reject) {
+      resolveFunc = resolve;
+      rejectFunc = reject;
+      initFunc(resolve, reject);
+    });
+    ret.resolve = function (value) {
+      abortFunc();
+      resolveFunc(value);
+    };
+    ret.reject = function (reason) {
+      abortFunc();
+      rejectFunc(reason);
+    };
+    return ret;
   };
-  ret.reject = function (reason) {
-    abortFunc();
-    rejectFunc(reason);
+  exports.base64Decode =  async function base64Decode(str) {
+    return await (new self.Blob([ self.atob(str) ])).arrayBuffer();
   };
-  return ret;
-};
-exports.base64Decode =  async function base64Decode(str) {
-  return await (new self.Blob([ self.atob(str) ])).arrayBuffer();
-};
-exports.base64Encode = function base64Encode(view) {
-  let rawString = "";
-  for (const byte of view) {
-    rawString += String.fromCharCode(byte);
-  }
-  return self.btoa(rawString);
-};
-// SiteStorage does not provide security, all variables are still accessible through the underlying storage object
-// If using SiteStorage, do not manipulate keys that start with an underscore or match RFC3986 syntax.
-exports.SiteStorage = class SiteStorage {
-  #uri;
-  #storage;
-  constructor(args) {
-    const { uri, storage } = args;
-    this.#uri = (new self.URL(uri)).toString();
-    this.#storage = storage;
-    let prefix = this.#storage.getItem(uri);
-    while (prefix === null) {
-      const view = new Uint8Array(12);
-      self.crypto.getRandomValues(view);
-      // 17-char prefix
-      prefix = "_" + base64Encode(view);
-      // unlikely, but check to make sure
-      if (this.#storage.getItem(prefix) !== null) {
-        prefix = null;
+  exports.base64Encode = function base64Encode(view) {
+    let rawString = "";
+    for (const byte of view) {
+      rawString += String.fromCharCode(byte);
+    }
+    return self.btoa(rawString);
+  };
+  // SiteStorage does not provide security, all variables are still accessible through the underlying storage object
+  // If using SiteStorage, do not manipulate keys that start with an underscore or match RFC3986 syntax.
+  exports.SiteStorage = class SiteStorage {
+    #uri;
+    #storage;
+    constructor(args) {
+      const { uri, storage } = args;
+      this.#uri = (new self.URL(uri)).toString();
+      this.#storage = storage;
+      let prefix = this.#storage.getItem(uri);
+      while (prefix === null) {
+        const view = new Uint8Array(12);
+        self.crypto.getRandomValues(view);
+        // 17-char prefix
+        prefix = "_" + base64Encode(view);
+        // unlikely, but check to make sure
+        if (this.#storage.getItem(prefix) !== null) {
+          prefix = null;
+        }
       }
+      this.#storage.setItem(prefix, uri);
+      this.#storage.setItem(uri, prefix);
     }
-    this.#storage.setItem(prefix, uri);
-    this.#storage.setItem(uri, prefix);
-  }
-  get(key) {
-    if (key === "") {
-      throw "empty string is not allowed";
+    get(key) {
+      if (key === "") {
+        throw "empty string is not allowed";
+      }
+      const prefix = this.#storage.getItem(this.#uri);
+      return this.#storage.getItem(prefix + key);
     }
-    const prefix = this.#storage.getItem(this.#uri);
-    return this.#storage.getItem(prefix + key);
-  }
-  set(key, item) {
-    if (key === "") {
-      throw "empty string is not allowed";
+    set(key, item) {
+      if (key === "") {
+        throw "empty string is not allowed";
+      }
+      const prefix = this.#storage.getItem(this.#uri);
+      this.#storage.setItem(prefix + key, item);
     }
-    const prefix = this.#storage.getItem(this.#uri);
-    this.#storage.setItem(prefix + key, item);
-  }
-  remove(key) {
-    if (key === "") {
-      throw "empty string is not allowed";
+    remove(key) {
+      if (key === "") {
+        throw "empty string is not allowed";
+      }
+      const prefix = this.#storage.getItem(this.#uri);
+      this.#storage.removeItem(prefix + key);
     }
-    const prefix = this.#storage.getItem(this.#uri);
-    this.#storage.removeItem(prefix + key);
-  }
-};
+  };
+  return exports;
+})();
