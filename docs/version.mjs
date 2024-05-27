bfa8caa5891a65e8fc3d793241c16c230433ccf6
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-05-27 14:18:57 -0400

    Update and rename versioning.js to version.mjs

---
+++ docs/version.mjs
@@ -0,0 +1,38 @@
+/*
+(c) 2024 Scot Watson  All Rights Reserved
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+import "SiteStorage.mjs"
+
+export function getVersionBaseUrl({
+  latestVersion,
+  siteURI
+}) {
+  const requestedVersion = exports.selfUrl.searchParams.get("version");
+  self._siteURI = siteURI;
+  window.siteSessionStorage = new SiteStorage({
+    uri: self._siteURI(),
+    storage: window.sessionStorage,
+  });
+  window.siteLocalStorage = new SiteStorage({
+    uri: self._siteURI(),
+    storage: window.localStorage,
+  });
+  const storedVersion = window.siteSessionStorage.get("version");
+  if (requestedVersion !== null) {
+    window.siteSessionStorage.set("version", requestedVersion);
+    const newSearchParams = new self.URLSearchParams(exports.selfUrl.searchParams.toString());
+    newSearchParams.delete("version");
+    const newURL = new self.URL(exports.selfUrl.protocol + "//" + exports.selfUrl.host + exports.selfUrl.pathname + ((newSearchParams.size === 0) ? "" : "?" + newSearchParams.toString()) + exports.selfUrl.hash);
+    window.history.replaceState(null, "", newURL.toString());
+  } else if (storedVersion !== null) {
+    window.siteSessionStorage.set("version", storedVersion);
+  } else {
+    window.siteSessionStorage.set("version", latestVersion);
+  }
+  self._version = function () {
+    return window.siteSessionStorage.get("version");
+  }
+  return "./" + self._version() + "/";
+}
