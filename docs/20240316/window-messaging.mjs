41d3296bd7bac04088f6794bf00dec5aa47e53b8
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-06-01 16:34:56 -0400

    Update window-messaging.mjs

--- docs/20240316/window-messaging.mjs
+++ docs/20240316/window-messaging.mjs
@@ -3,6 +3,18 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+// As of the latest revision of this file, W3C "Service Workers" is a W3C Candidate Recommendation Draft, last published 12 July 2022
+// https://www.w3.org/TR/2022/CRD-service-workers-20220712/
+// W3C "Service Workers" also has an Editor's Draft, published 30 April 2024
+// https://w3c.github.io/ServiceWorker/
+// According to section 2.1.1 of W3C "Service Workers":
+// A user agent may terminate service workers at any time it:
+//  - Has no event to handle.
+//  - Detects abnormal operation: such as infinite loops and tasks exceeding imposed time limits (if any) while handling the events.
+//
+// Despite this, Firefox terminates the service worker, even when sending messages to itself (therefore handling message events), with a short handler routine.
+// However, message events received from a Window do prevent the service worker from terminating.
+
 import * as Common from "https://scotwatson.github.io/WebInterface/common.mjs";
 import * as MessagingCommon from "https://scotwatson.github.io/WebInterface/messaging-common.mjs";
 
@@ -143,3 +155,38 @@ export function createMessageSinkForServiceWorker(serviceWorker) {
     },
   };
 }
+
+const serviceWorkerHeartbeats = new Map();
+export function setServiceWorkerHeartbeat({
+  serviceWorker,
+  interval, // in ms
+}) {
+  let intervalID = serviceWorkerHeartbeats.get(serviceWorker);
+  if (intervalID) {
+    clearInterval(intervalID);
+    serviceWorkerHeartbeats.remove(serviceWorker);
+  }
+  if (interval !== 0) {
+    intervalID = setInterval(() => {
+      serviceWorker.postMessage({ action: "ping" });
+    }, interval);
+    serviceWorkerHeartbeats.set(serviceWorker, intervalID);
+  }
+}
+
+export const controllerchange = Common.createSignal((resolve, reject) => {
+  navigator.serviceWorker.addEventListener("controllerchange", (evt) => {
+    resolve({
+      serviceWorker: self.navigator.serviceWorker.controller,
+      messageSource: Messaging.controllerSource,
+      messageSink: Messaging.createMessageSinkForServiceWorker(self.navigator.serviceWorker.controller),
+    });
+  });
+  if (navigator.serviceWorker.controller !== null) {
+    resolve({
+      serviceWorker: self.navigator.serviceWorker.controller,
+      messageSource: Messaging.controllerSource,
+      messageSink: Messaging.createMessageSinkForServiceWorker(self.navigator.serviceWorker.controller),
+    });
+  }
+});
