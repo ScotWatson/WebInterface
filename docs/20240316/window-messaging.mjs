594aecae7ecfcea93ab8f187105d6a5d9006145f
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-06-03 18:17:47 -0400

    Update window-messaging.mjs

--- docs/20240316/window-messaging.mjs
+++ docs/20240316/window-messaging.mjs
@@ -42,6 +42,7 @@ export const untrustedOrigin = Common.createSignal(function (resolve, reject) {
 });
 
 export function messageHandler(evt) {
+  console.log(evt);
   if (evt.source === null) {
     // Should only occur on MessagePorts and Workers
     throw "Internal Logic Error";
