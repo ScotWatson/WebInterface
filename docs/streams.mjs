19a2a6416e3fe2b410cae7eab7f5c538ab0dcfc4
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-06-12 22:44:38 -0400

    Update streams.mjs

--- docs/streams.mjs
+++ docs/streams.mjs
@@ -3,6 +3,11 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+// An active source is implemented as an async iterator
+// A passive source is implemented as an iterator
+// A passive sink is implemeted as a function that takes a single parameter and returns undefined
+// There is no implemetation of an active sink. Active sinks are given a queue of their own and get implemented as passive sinks
+
 export class ActiveSource {
   // function init is expected to be a function that takes three arguments (resolve, reject, return), which each are functions that take a single argument.
   // Each time resolve is called, every iterator instance resolves with a structured clone of the value passed (therefore, the value must be clonable).
@@ -58,43 +63,6 @@ export class ActiveSource {
   }
 };
 
-export class ActiveSink {
-  // initFunc is expected to be a function that takes one argument (dispatch), which is a function that takes a single argument.
-  // Each time dispatch is called, every iterator instance resolves with a structured clone of the value passed (therefore, the value must be clonable).
-  #inputRequest;
-  #nextRequest;
-  constructor(init) {
-    const nextRequest = () => {
-      this.#nextRequest = return new Promise((resolve, reject) => {
-        this.#inputRequest = resolve;
-      });
-    };
-    nextRequest();
-    this.#parse = initFunc(
-      /* dispatch */() => {
-        internal.inputRequest();
-        nextInput();
-      },
-    );
-  }
-  async *[Symbol.asyncIterator]() {
-    try {
-      let value;
-      let done = false;
-      while (!done) {
-        await this.#nextRequest;
-        done = !!this.#parse(yield);
-      }
-      return value;
-    } catch (e) {
-      // Error has been thrown
-      throw e;
-    } finally {
-      // Perform any cleanup
-    }
-  }
-};
-
 class OperationSource {  // Passive Source
   #state;
   constructor(operation) {
@@ -127,230 +95,79 @@ class OperationSink {  // Passive Sink
   }
 }
 
-function connectPump() {
-  // Create Pump
-  let nextRequest = new Promise((resolve, reject) => {
-    setTimeout(resolve, 100);
-  });
-  let inputResolve;
-  let inputReject;
-  let nextInput = new Promise((resolve, reject) => {
-    inputResolve = resolve;
-    inputReject = reject;
-  });
-  const intervalId = setInterval(() => {
-    const { value, done } = iterator.next();
-    if (done) {
-      clearInterval(intervalId);
-    }
-    if (value !== undefined) {
-      sink(value);
-    }
-  }, 0);
-  parse();
-  const obj = {
-    stop() {
-      clearInterval(intervalId);
-    },
-    async *input() {
-      try {
-        let value;
-        let done = false;
-        while (!done) {
-          await nextRequest;
-          done = !!inputResolve({
-            value: yield,
-            done: false,
-          });
-        }
-        return value;
-      } catch (e) {
-        // Error has been thrown
-        throw e;
-      } finally {
-        // Perform any cleanup
-      }
-    }
-    async *output() {
-      try {
-        let value;
-        let done = false;
-        ({ value, done } = await nextInput;
-        while (!done) {
-          yield self.structuredClone(value);
-          ({ value, done } = await nextInput;
-        }
-        return value;
-      } catch (e) {
-        // Error has been thrown
-        throw e;
-      } finally {
-        // Perform any cleanup
-      }
-    }
-  };
-}
-
-{
-  *[Symbol.iterator]() {
-    while (true) {
-      
-    }
-  }
-}
 function waitResolve(value, ms) {
   return new Promise((resolve) => { setTimeout(() => { resolve(value); }, ms); });
 }
-(() => {
-  const asyncIterator = (async *() {
-    const init = 1;
-    let input;
-    let output = init;
-    while (true) {
-      input = yield waitResolve(output, 1000);
-      if (input > 10) {
-        return waitResolve(input - 1, 1000);
-      }
-      output = input * 2;
-    }
-  })();
-  const activeProcess = asyncIterator.next;
-  const passiveProcess = (input) => {
-    return { value: input + 2, done: false, };
-  };
-  const pipe = (async () => {
-    let input = await activeProcess();
-    while (true) {
-      if (!!input.done) { break; }
-      const output = passiveProcess(input.value);
-      if (!!output.done) { break; }
-      input = await activeProcess(output.value);
-    }
-  })();
-  pipe.then(() => { console.log("pipe closed"); });
-  const pipeActiveToPassive = (async () => {
-    let forward = await activeProcess();
-    while (true) {
-      if (!!forward.done) { break; }
-      const reverse = passiveProcess(input.value);
-      if (!!reverse.done) { break; }
-      forward = await activeProcess();
-    }
-  })();
-  const pipePassiveToActive = (async () => {
-    let reverse = await activeProcess();
-    while (true) {
-      if (!!reverse.done) { break; }
-      const forward = passiveProcess();
-      if (!!forward.done) { break; }
-      reverse = await activeProcess(forward.value);
-    }
-  })();
-})();
-(() => {
-  const iterator = (*() {
-    const init = 1;
-    let input;
-    let output = init;
-    while (true) {
-      input = yield output;
-      if (input > 10) {
-        return input - 1;
-      }
-      output = input * 2;
-    }
-  })();
-  const passiveProcess = iterator.next;
-  const activeProcess = async (input) => {
-    return await waitResolve({ value: input + 2, done: false, });
-  };
-  const pipe = (async () => {
-    let input = passiveProcess();
-    while (true) {
-      if (!!input.done) { break; }
-      const output = await activeProcess(input.value);
-      if (!!output.done) { break; }
-      input = passiveProcess(output.value);
-    }
-  })();
-  pipe.then(() => { console.log("pipe closed"); });
-  const pipePassiveToActive = (async () => {
-    let forward = passiveProcess();
-    while (true) {
-      if (!!forward.done) { break; }
-      const reverse = await activeProcess(forward.value);
-      if (!!reverse.done) { break; }
-      forward = passiveProcess();
-    }
-  })();
-  const pipeActiveToPassive = (async () => {
-    let reverse = passiveProcess();
-    while (true) {
-      if (!!reverse.done) { break; }
-      const forward = await activeProcess();
-      if (!!forward.done) { break; }
-      reverse = passiveProcess(forward.value);
-    }
-  })();
-})();
 
-  const pipeActiveToPassive = (async () => {
-    let forward = await activeProcess();
-    while (true) {
-      if (!!forward.done) { break; }
-      const reverse = passiveProcess(forward.value);
-      if (!!reverse.done) { break; }
-      forward = await activeProcess();
-    }
-  })();
-/*
-  const pipeActiveToPassive = (async () => {
-    let reverse = passiveProcess();
-    while (true) {
-      if (!!reverse.done) { break; }
-      const forward = await activeProcess();
-      if (!!forward.done) { break; }
-      reverse = passiveProcess(forward.value);
-    }
-  })();
-  */
-  const pipePassiveToActive = (async () => {
-    let reverse = await activeProcess();
-    while (true) {
-      if (!!reverse.done) { break; }
-      const forward = passiveProcess();
-      if (!!forward.done) { break; }
-      reverse = await activeProcess(forward.value);
-    }
-  })();
-/*
-  const pipePassiveToActive = (async () => {
-    let forward = passiveProcess();
-    while (true) {
-      if (!!forward.done) { break; }
-      const reverse = await activeProcess(forward.value);
-      if (!!reverse.done) { break; }
-      forward = passiveProcess();
-    }
-  })();
-  */
-
-export function pipeActiveToPassive(source, sink) {
-  const sourceObj = (function () {
-    if ((source === "function") && (source.constructor.name === "AsyncGenerator")) {
+export class TransformOperation {
+  constructor(operation) {
     
-  } else if (typeof source[Symbol.asyncIterator] === "function")) {
-  } else {
-    throw "source is not an active source";
   }
-  if (typeof sink[Symbol.iterator] !== "function") {
+}
+export class SourceTransform {
+  constructor(passiveSource) {
+    
+  }
+}
+export class SourcePump {
+  #source;
+  constructor(passiveSource) {
+    this.#source = getPassiveSource(passiveSource);
+    
+    setInterval(() => {
+      const forward = this.#source.next();
+      send(forward.value);
+    }, 100);
+  }
+  async *[Symbol.asyncIterator]() {
+    
+  }
+}
+export class Pipe {
+  #source;
+  #sink;
+  constructor(activeSource, passiveSink) {
+    this.#source = getActiveSource(activeSource);
+    this.#sink = getPassiveSink(passiveSink);
+    this.done = (async () => {
+      forward = await this.#source();
+      while (!forward.done) {
+        this.#sink(forward.value);
+        forward = await this.#source();
+      }
+    })();
+  }
+}
+
+function getActiveSource(obj) {
+  if (isAsyncGenerator(obj)) {
+    return obj();
+  } else if (typeof obj === "object" && obj !== null && isAsyncGenerator(obj[Symbol.asyncIterator])) {
+    return obj[Symbol.asyncIterator]();
+  } else {
+    throw "Not an active source";
+  }
+  function isAsyncGenerator(obj) {
+    return (typeof obj === "function") && (obj.prototype.constructor.toString() === "[object AsyncGenerator]")
+  }
+}
+function getPassiveSource(obj) {
+  if (isGenerator(obj)) {
+    return obj();
+  } else if (typeof obj === "object" && obj !== null && isGenerator(obj[Symbol.iterator])) {
+    return obj[Symbol.iterator]();
+  } else {
+    throw "Not a passive source";
+  }
+  function isGenerator(obj) {
+    return (typeof obj === "function") && (obj.prototype.constructor.toString() === "[object Generator]")
+  }
+}
+function getPassiveSink(obj) {
+  if (typeof obj !== "function") {
     throw "sink is not a passive sink.";
   }
-  (async () => {
-    for await (const input of source) {
-      sink(input);
-    }
-  })();
+  return obj;
 }
 
 export function pipePassiveToActive(source, sink) {
