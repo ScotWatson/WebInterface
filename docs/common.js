534e3ab24dc7cfad22a0a292d0891fed4e04166d
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-06-19 19:47:50 -0400

    Update common.js

--- docs/common.js
+++ docs/common.js
@@ -7,7 +7,6 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 (self.document === undefined ? self : self.document).currentScript.exports = (function () {
   const exports = {};
-  // async Iterable Iterator
   exports.base64Decode =  async function base64Decode(str) {
     return await (new self.Blob([ self.atob(str) ])).arrayBuffer();
   };
@@ -17,51 +16,6 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
       rawString += String.fromCharCode(byte);
     }
     return self.btoa(rawString);
-  };
-  // SiteStorage does not provide security, all variables are still accessible through the underlying storage object
-  // If using SiteStorage, do not manipulate keys that start with an underscore or match RFC3986 syntax.
-  exports.SiteStorage = class SiteStorage {
-    #uri;
-    #storage;
-    constructor(args) {
-      const { uri, storage } = args;
-      this.#uri = (new self.URL(uri)).toString();
-      this.#storage = storage;
-      let prefix = this.#storage.getItem(uri);
-      while (prefix === null) {
-        const view = new Uint8Array(12);
-        self.crypto.getRandomValues(view);
-        // 17-char prefix
-        prefix = "_" + base64Encode(view);
-        // unlikely, but check to make sure
-        if (this.#storage.getItem(prefix) !== null) {
-          prefix = null;
-        }
-      }
-      this.#storage.setItem(prefix, uri);
-      this.#storage.setItem(uri, prefix);
-    }
-    get(key) {
-      if (key === "") {
-        throw "empty string is not allowed";
-      }
-      const prefix = this.#storage.getItem(this.#uri);
-      return this.#storage.getItem(prefix + key);
-    }
-    set(key, item) {
-      if (key === "") {
-        throw "empty string is not allowed";
-      }
-      const prefix = this.#storage.getItem(this.#uri);
-      this.#storage.setItem(prefix + key, item);
-    }
-    remove(key) {
-      if (key === "") {
-        throw "empty string is not allowed";
-      }
-      const prefix = this.#storage.getItem(this.#uri);
-      this.#storage.removeItem(prefix + key);
-    }
   };
   return exports;
 })();
