78f7ee9d1067e8a4b86eacb8d2818bdc77620784
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-06-22 09:45:47 -0400

    Update common.mjs

--- docs/common.mjs
+++ docs/common.mjs
@@ -3,6 +3,9 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+export * as Streams from "https://scotwatson.github.io/WebInterface/streams.mjs";
+export { default as RemoteProcedureSocket } from "https://scotwatson.github.io/WebInterface/RemoteProcedureSocket.mjs";
+
 export async function base64Decode(str) {
   return await (new self.Blob([ self.atob(str) ])).arrayBuffer();
 };
