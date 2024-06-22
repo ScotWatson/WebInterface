1b4fd6f02884e594738b6961436484da480a33cb
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-06-21 21:11:17 -0400

    Update message-socket.mjs

--- docs/message-socket.mjs
+++ docs/message-socket.mjs
@@ -3,22 +3,25 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-import * as Common from "https://scotwatson.github.io/WebInterface/common.mjs";
+import * as Streams from "https://scotwatson.github.io/WebInterface/streams.mjs";
 import MessageQueue from "https://scotwatson.github.io/WebInterface/MessageQueue.mjs";
 
 function forMessagePort(messagePort) {
   const queue = new MessageQueue(messagePort);
   messagePort.start();
   return {
-    message: Common.createSignal(function (resolve, reject) {
+    output: new Streams.ActiveSource(function (resolve, reject) {
       messagePort.addEventListener("message", (evt) => resolve(evt.data) );
     }),
-    send: function ({
-      data,
-      transfer,
-    }) {
-      messagePort.postMessage(data, transfer);
-    },
+    input: new Sink((data) {
+      if ((typeof data === "object") && Object.has(data, "_transfer")) {
+        const transfer = data._transfer;
+        delete data._transfer;
+        messagePort.postMessage(data, transfer);
+      } else {
+        messagePort.postMessage(data);
+      }
+    }),
     start() {
       queue.start();
     },
