d70b547ccdd4371652c2bee4755a244b9eed7c30
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-06-23 17:28:47 -0400

    Update RemoteProcedureSocket.mjs

--- docs/RemoteProcedureSocket.mjs
+++ docs/RemoteProcedureSocket.mjs
@@ -10,9 +10,11 @@ export default class RemoteProcedureSocket {
   #responseFunctions;
   #resolve;
   #reject;
+  #timeout;
   constructor({
     timeout, // in ms
   }) {
+    this.#timeout = timeout;
     this.#packetIds = new Map();
     this.#responseFunctions = new Map();
     this.input = new Streams.SinkNode((data) => {
@@ -69,8 +71,8 @@ export default class RemoteProcedureSocket {
     const packetId = self.crypto.randomUUID();
     const requesting = new Promise(function (resolve, reject) {
       packetIds.set(packetId, { resolve, reject });
-      if (timeout) {
-        self.setTimeout(rejectOnTimeout, timeout);
+      if (this.#timeout) {
+        self.setTimeout(rejectOnTimeout, this.#timeout);
         function rejectOnTimeout() {
           reject("Request Timed Out: " + packetId);
         }
