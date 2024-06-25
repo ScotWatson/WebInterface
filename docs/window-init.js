e8b3683115bdaaa36a73f18ecc7057b75bcd1a75
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-06-25 18:24:23 -0400

    Update and rename init.js to window-init.js

---
+++ docs/window-init.js
@@ -0,0 +1,50 @@
+/*
+(c) 2024 Scot Watson  All Rights Reserved
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+// Intended to be included as the first script tag in an HTML file
+// Provides functions useful for initializing a web page
+
+"use strict";
+
+(self.document === undefined ? self : self.document).currentScript.exports = (function () {
+  const exports = {};
+  // Resolves once the DOM is fully parsed and all scripts have finish execution
+  exports.contentLoaded = new Promise(function (resolve, reject) {
+    if ((document.readyState === "loaded") || (document.readyState === "complete")) {
+      resolve(evt);
+    }
+    document.addEventListener("DOMContentLoaded", function (evt) {
+      resolve(evt);
+    });
+    // This is a fallback in case the browser does not support readyState === "loaded" and the promise is created between the end of script execution and the loading all resources.
+    window.addEventListener("load", function (evt) {
+      resolve(evt);
+    });
+  });
+  // Resolves once all resources have been fully loaded
+  exports.load = new Promise(function (resolve, reject) {
+    if (document.readyState === "complete") {
+      resolve(evt);
+    }
+    window.addEventListener("load", function (evt) {
+      resolve(evt);
+    });
+  });
+  // Resolves once the page is under the control of a ServiceWorker
+  exports.controller = new Promise(function (resolve, reject) {
+    if (self.navigator.serviceWorker.controller !== null) {
+      resolve();
+      return;
+    }
+    self.navigator.serviceWorker.addEventListener("controllerchange", function (evt) {
+      resolve();
+      return;
+    });
+  });
+  // Obtain initialization info
+  exports.selfUrl = self.location;
+  
+  return exports;
+})();
