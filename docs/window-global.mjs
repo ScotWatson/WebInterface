1dbb8a9e7469968c68dd08d4637a519affee363e
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-06-28 17:20:12 -0400

    Update window-global.mjs

--- docs/window-global.mjs
+++ docs/window-global.mjs
@@ -13,99 +13,11 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 // However, message events received from a Window do prevent the service worker from terminating.
 
 import * as Common from "https://scotwatson.github.io/WebInterface/common.mjs";
+import * as Resources from "https://sctwatson.github.io/WebInterface/resources.mjs"
 
 export * as Common from "https://scotwatson.github.io/WebInterface/common.mjs";
 export { default as SiteStorage } from "https://scotwatson.github.io/WebInterface/SiteStorage.mjs";
 
-export function addShortcutIcon() {
-  const link = document.createElement("link");
-  link.rel = "shortcut icon";
-  link.href = "./favicon.ico";
-  link.type = "image/x-icon";
-  document.head.appendChild(link);
-}
-export function addIcon() {
-  const link = document.createElement("link");
-  link.rel = "icon";
-  link.href = "./favicon.ico";
-  link.type = "image/x-icon";
-  document.head.appendChild(link);
-}
-// Adds a stylesheet
-export function addStyleSheet(url) {
-  const style = document.createElement("link");
-  style.href = url;
-  style.rel = "stylesheet";
-}
-// Blocks HTML parsing until script is fetched and parsed
-export function addSyncScript(url) {
-  const script = document.createElement("script");
-  script.src = url;
-  document.head.appendChild(script);
-  return {
-    url,
-    loading: new Promise(function (resolve, reject) {
-      script.addEventListener("load", load);
-      script.addEventListener("error", error);
-      function load(evt) {
-        script.removeEventListener("load", load);
-        script.removeEventListener("error", error);
-        resolve(script);
-      }
-      function error(evt) {
-        script.removeEventListener("load", load);
-        script.removeEventListener("error", error);
-        reject(evt);
-      }
-    }),
-  };
-}
-// Fetches script with blocking HTML parser
-export function addScript({
-  url,  // location of script
-  defer,  // does script execution wait until after HTML is parsed
-  asModule,  // is script executed as a module
-}) {
-  const script = document.createElement("script");
-  script.src = url;
-  if (asModule) {
-    script.type = "module";
-    if (!defer) {
-      script.async = "true";
-    }
-  } else {
-    script.defer = !!defer;
-  }
-  document.head.appendChild(script);
-  return {
-    url,
-    loading: new Promise(function (resolve, reject) {
-      script.addEventListener("load", load);
-      script.addEventListener("error", error);
-      function load(evt) {
-        script.removeEventListener("load", load);
-        script.removeEventListener("error", error);
-        resolve(script);
-      }
-      function error(evt) {
-        script.removeEventListener("load", load);
-        script.removeEventListener("error", error);
-        reject(evt);
-      }
-    }),
-  };
-}
-// Resolves once the DOM is fully parsed
-export const interactive = new Promise(function (resolve, reject) {
-  if ((document.readyState === "interactive") || (document.readyState === "complete")) {
-    resolve(evt);
-  }
-  document.addEventListener("readystatechange", function (evt) {
-    if (document.readyState === "interactive") {
-      resolve(evt);
-    }
-  });
-});
 
 function postMessage(messagePort, origin, data) {
   const transfer = getTransfer(data);
