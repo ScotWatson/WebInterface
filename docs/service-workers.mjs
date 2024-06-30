f011c78f8342202a9dc2771d7989850b2dc01088
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-06-29 20:13:27 -0400

    Update service-workers.mjs

--- docs/service-workers.mjs
+++ docs/service-workers.mjs
@@ -178,7 +178,7 @@ class ServiceWorker {
 }
 
 export const controllerSource = new Streams.SourceNode((resolve, reject) => {
-  self.navigator.serviceWorker.addEventListener("message", (x) => { console.log(x); resolve(x); });
+  self.navigator.serviceWorker.addEventListener("message", (evt) => { resolve(evt.data); });
 });
 export const controllerSink = new Streams.SinkNode((data) => {
   MessageNode.postMessage(self.navigator.serviceWorker.controller, data);
