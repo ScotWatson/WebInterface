beb467d3a83d91fd005d04093df1d43bee5b7171
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-06-29 21:00:16 -0400

    Update RPCNode.js

--- docs/RPCNode.js
+++ docs/RPCNode.js
@@ -119,6 +119,9 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
         verb,
         args,
       });
+      if (!verb) {
+        throw "Invalid Verb";
+      }
       const callId = self.crypto.randomUUID();
       const requesting = new Promise((resolve, reject) => {
         this.#callIds.set(callId, { resolve, reject });
